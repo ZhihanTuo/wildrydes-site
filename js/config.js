969c085b407223368f87f467773ed8caaddb3a4b
Zhihan Tuo <johntuozh@gmail.com>
2024-04-18 15:22:33 -0700

    new config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_kLWZS0xEg', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'op0lglhhraa9u08003mfgma5d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
