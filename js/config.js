97248593867d63aa9b2847b0bafc52dd737f5452
Zhihan Tuo <johntuozh@gmail.com>
2024-05-13 18:47:44 -0700

    Modified user ids

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_kLWZS0xEg', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'op0lglhhraa9u08003mfgma5d', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_dzleubhGc', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'nig57ougp6ic5e2i1sq5svklp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
